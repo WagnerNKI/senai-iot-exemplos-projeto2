125e1774e09dd658c9d5387ef80446b99af47601
INFO\32658368860 <wagnwnki@gmail.com>
2017-10-26 21:50:43 -0200

    wag

--- Exemplo_Sensor_Ultrassonico/Exemplo_Sensor_Ultrassonico.ino
+++ Exemplo_Sensor_Ultrassonico/Exemplo_Sensor_Ultrassonico.ino
@@ -2,14 +2,31 @@
 // Essa biblioteca não vem por padrão com o arduino e 
 // precisa ser instalada pelo gerenciador de bibliotecas
 #include <Ultrasonic.h>
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Inicializa o objeto do sensor ultrasônico
 // Usando as portas 12 e 13 para trigger e echo
-Ultrasonic ultrasonic(12, 13);
+Ultrasonic ultrasonic(7, 6);
+int ledRed = 13;
+int ledYellow =9;
+int buzz = 10;
 
 void setup() {
   // Inicializa a porta Serial
   Serial.begin(9600);
+  pinMode ( ledYellow , OUTPUT);
+  pinMode ( ledRed , OUTPUT);
+  pinMode ( buzz , OUTPUT);
+    // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
 
 void loop() {
@@ -19,6 +36,27 @@ void loop() {
   // Escreve o valor da distância no painel Serial
   Serial.print("Distance in CM: ");
   Serial.println(distancia);
-  
-  delay(1000);
+
+  if (distancia <= 5){
+    digitalWrite(ledRed,HIGH);
+    digitalWrite(ledYellow,LOW);
+
+    
+  }
+  else if(distancia > 5 && distancia <= 30){
+  digitalWrite(ledYellow,HIGH);
+  digitalWrite(ledRed,LOW);
+
+  }
+ else{
+  digitalWrite(ledRed,LOW);
+  digitalWrite(ledYellow,LOW);
+ }
+   // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(0, 1);
+  lcd.print(distancia);
+  delay(500);
 }
